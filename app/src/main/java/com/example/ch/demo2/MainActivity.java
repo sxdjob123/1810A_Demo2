6c2dc4fa48f0faf389d53dfac511d2aecdfb288e
sxdjob123 <761651982@qq.com>
2019-05-29 15:23:15 +0800

    第一次提交项目

--- app/src/main/java/com/example/ch/demo2/MainActivity.java
+++ app/src/main/java/com/example/ch/demo2/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.ch.demo2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * 大牛养成记
+     */
+    private TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mTv = (TextView) findViewById(R.id.tv);
     }
 }
