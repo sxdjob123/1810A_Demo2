9160a82b5655b2cbf3a8fa7b55a1510994f57f4b
sxdjob123 <761651982@qq.com>
2019-05-29 16:56:24 +0800

    提交项目

--- app/src/main/java/com/example/ch/demo2/MainActivity.java
+++ app/src/main/java/com/example/ch/demo2/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.ch.demo2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     /**
      * 大牛养成记
      */
     private TextView mTv;
+    /**
+     * 跳转
+     */
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mTv = (TextView) findViewById(R.id.tv);
+        mBtn = (Button) findViewById(R.id.btn);
+        mBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.btn:
+                break;
+        }
     }
 }
